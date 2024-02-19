d7425d809dbcbba54c58e6d2a0562a281a938265
Thomas3300000 <8844@holbertonstudents.com>
2024-02-19 10:15:50 +0000

    puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (void) 
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("i"Programming is like building a multilingual puzzle");
 
 	return 0;
 }	
