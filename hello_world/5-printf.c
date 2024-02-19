c29f62f317379ed61b632519fc752f1259409d99
Thomas3300000 <8844@holbertonstudents.com>
2024-02-19 13:38:02 +0000

    printsf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,9 +4,10 @@
  * main - Prints: with proper grammar, but the outcome is a piece of art,
  *
  * Return: 0
- */ 
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);	
+	return (0);
+}
