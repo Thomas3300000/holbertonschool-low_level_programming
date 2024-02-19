861f963817a3dd07d559bf9ae622be38f56cdfef
Thomas3300000 <8844@holbertonstudents.com>
2024-02-19 13:14:19 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include <stdio.h> 
+#include <stdio.h>
+
 /**
-* main - Prints \"Programming is like building a multilingual puzzle 
-*
-* Return : 0
-*/   
+ * main - Prints \"Programming is like building a multilingual puzzle
+ *
+ * Return: 0
+ */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
