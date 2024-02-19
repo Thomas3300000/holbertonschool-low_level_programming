959732896096b98a6b4919ef831ddfae7a80bb71
Thomas3300000 <8844@holbertonstudents.com>
2024-02-19 11:00:17 +0000

    puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 
-/* imprime un message avec la fonction puts */
 {
+	/* imprime un message avec la fonction puts */
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
