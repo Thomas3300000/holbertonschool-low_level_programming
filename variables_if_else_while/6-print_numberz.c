605987733d1ee28cd44d45bcb5940ffc2c3bc23a
Thomas3300000 <8844@holbertonstudents.com>
2024-02-20 16:19:37 +0000

    num

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all single digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+
+	putchar('\n');
+	return (0);
+}
