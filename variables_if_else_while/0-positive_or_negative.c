d984f499eb4214fdcb5c6ebd2d69fda0d4fbeb0d
Thomas3300000 <8844@holbertonstudents.com>
2024-02-20 09:01:58 +0000

    positive negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Affecter un nombre aleatoire a n
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2
+
+	if (n > 0)
+		printf("%d est positif\n", n);
+	else if (n < 0)
+		printf("%d est nÃgatif\n", n);
+	else
+		printf("%d est zÃro\n", n);
+
+	return (0);
+}
