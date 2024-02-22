3b72b282bc2069cab45f691ece2ae38d435cda64
Thomas3300000 <8844@holbertonstudents.com>
2024-02-22 16:46:29 +0000

    save 8

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -17,10 +17,8 @@ int _abs(int n)
 
 	if (n > 0)
 	{
-		n = 1 * n;
 		return (n);
 	}
-
 	else
 		return (0);
 
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  prints every minute of the day of Jack Bauer,00:00 to 23:59.
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+
+	for (h1 = '0'; h1 <= '2'; h1++)
+	{
+		_putchar(h1);
+	}
+
+		for (h2 = '0'; h2 <= '9'; h2++)
+		{
+			{
+				if (h1 == '2' )
+				for (h2 = '0'; h2 <= '3'; h2++)
+					_putchar(h2);
+
+
+			}
+			
+			_putchar(h2);
+		}
+
+		_putchar('\n');
+
+			for (m1 = '0'; m1 <= '5'; m1++)
+			{
+			_putchar(m1);
+			}
+			
+			for (m2 = '0'; m2 <= '9'; m2++)
+			{
+			_putchar(m2);
+			}
+			
+			_putchar('\n');
+}
