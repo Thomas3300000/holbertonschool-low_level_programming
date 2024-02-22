f18e998674b1408937055c1e169311c8baf9d721
Thomas3300000 <8844@holbertonstudents.com>
2024-02-22 19:10:58 +0000

    jack

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -14,33 +14,23 @@ void jack_bauer(void)
 
 	for (h1 = '0'; h1 <= '2'; h1++)
 	{
-		_putchar(h1);
-	}
-
 		for (h2 = '0'; h2 <= '9'; h2++)
 		{
+			if ((h1 <= '1' && h2 <= '9') || (h1 == '2' && h2 <= '3'))
 			{
-				if (h1 == '2' )
-				for (h2 = '0'; h2 <= '3'; h2++)
-					_putchar(h2);
-
-
+				for (m1 = '0'; m1 <= '5'; m1++)
+				{
+					for (m2 = '0'; m2 <= '9'; m2++)
+					{
+						_putchar(h1);
+						_putchar(h2);
+						_putchar(':');
+						_putchar(m1);
+						_putchar(m2);
+						_putchar('\n');
+					}
+				}
 			}
-			
-			_putchar(h2);
 		}
-
-		_putchar('\n');
-
-			for (m1 = '0'; m1 <= '5'; m1++)
-			{
-			_putchar(m1);
-			}
-			
-			for (m2 = '0'; m2 <= '9'; m2++)
-			{
-			_putchar(m2);
-			}
-			
-			_putchar('\n');
+	}
 }
