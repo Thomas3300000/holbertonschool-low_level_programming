1ce4f1cdd132c9a5c5168879ceba7c127d8f2d3d
Thomas3300000 <8844@holbertonstudents.com>
2024-02-26 09:54:19 +0000

    update

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _isupper -  function that checks for uppercase character.
+ * @c: letters checked.
+ *
+ * Return: 0 for lowercase, 1 for uppercase.
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Task 0 : fonction qui vérifie la présence d'un caractère majuscule.
