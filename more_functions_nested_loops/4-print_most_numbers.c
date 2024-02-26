3c5cfb96e2b59c72e41189ec32d5ed4d0073fd1b
Thomas3300000 <8844@holbertonstudents.com>
2024-02-26 14:14:53 +0000

    numbers !=2 !=4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: 0.
+ */
+
+void print_most_numbers(void)
+{
+	int num = 0 ;
+	
+		if (num != '2' && num != '4')
+		{
+			 for (num = 0; num <= 9; num++)
+				 _putchar(num + '0');
+		}
+
+		_putchar('\n');
+	
+}
+
+
