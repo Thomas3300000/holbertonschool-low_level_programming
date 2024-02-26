ee19c8885665dae707b3cdeeba673da378c13471
Thomas3300000 <8844@holbertonstudents.com>
2024-02-26 22:00:06 +0000

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main  - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+	for (num = 1; num <= 100; num++)
+	{
+		if (num != 100)
+			printf(" ");
+		if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else if ((num % 3 == 0) && (num % 5 == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
+
