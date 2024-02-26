9ec68d1e75ac1b2d2594fc0b43344b27ae5babd5
Thomas3300000 <8844@holbertonstudents.com>
2024-02-26 16:06:04 +0000

    line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - check the code
+ * @n: 
+ *
+ * Return: 0.
+ */
+void print_line(int n)
+{
+	int line;
+
+	for (line = 0; line < n; line++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}	
