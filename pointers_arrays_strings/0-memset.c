49f640bb75018d258524c1378e54f78bae0b3476
Thomas3300000 <8844@holbertonstudents.com>
2024-03-04 13:04:28 +0000

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
+ * *_memset - fonction qui remplit la m√©moire avec un octet constat
+ * @s: zone point√e
+ * @b: byte
+ * @n: amount
+ * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
