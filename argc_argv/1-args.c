6c22ebb049bab40f11dbd92ff397c6dbbbc464e8
Thomas3300000 <8844@holbertonstudents.com>
2024-03-08 12:30:53 +0000

    name

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints the number of arguments passed into it
+ * @argc: arguments
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d",argc - 1);
+	return (0);
+}
