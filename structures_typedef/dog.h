9bafaf6cdb9cee07fd464ed53003cd00b958c64c
Thomas3300000 <8844@holbertonstudents.com>
2024-03-15 09:00:56 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * struct dog - create a dog
+ * @name: name a dog
+ * @age: age a dog
+ * @owner: owner a dog
+ * */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
