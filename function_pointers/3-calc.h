2e0a14fd4617d0758d2baddce22a6fe3769caac7
Thomas3300000 <8844@holbertonstudents.com>
2024-03-22 12:14:56 +0000

    calc operation

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -2,6 +2,7 @@
 #define CALC_H
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct op - Struct op
  *
@@ -10,8 +11,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 
