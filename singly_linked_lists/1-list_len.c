a0168e96e5d4e01835d079a9c6b55cdc2c4d7510
Thomas3300000 <8844@holbertonstudents.com>
2024-03-31 21:57:53 +0000

    Fix trailing space line 4

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - function that returns the number of elements 
+ * list_len - function that returns the number of elements
  * in a linked list_t list
  * @h: linked list
  *
