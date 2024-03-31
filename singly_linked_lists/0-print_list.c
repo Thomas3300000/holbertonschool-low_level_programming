756c90a0076bb33ad7b24653f67cd26789d13846
Thomas3300000 <8844@holbertonstudents.com>
2024-03-31 21:21:10 +0000

    create all files and make print_list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+ /**
+  *
+  *
+  *
+  */
+size_t print_list(const list_t *h)
+{
+	size_t cpt = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
+		cpt++;
+	}
+	return (cpt);
+}
