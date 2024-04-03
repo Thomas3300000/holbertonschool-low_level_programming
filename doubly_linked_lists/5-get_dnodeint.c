1c595af5fe949fb89703b943716a2ac77b1ea6fe
Thomas3300000 <8844@holbertonstudents.com>
2024-04-03 15:27:04 +0000

    delete condition if useless

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-/**
+/*u*
  * get_dnodeint_at_index - function that returns the nth node
  * of a dlistint_t linked list
  * @head: pointer to the head to the linked list
@@ -14,11 +14,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	for (cpt = 0; head != NULL && cpt < index; cpt++)
 	{
-		if (head == NULL)
-		{
-			return (NULL);
-		}
-
 		head = head->next;
 	}
 	return (head);
