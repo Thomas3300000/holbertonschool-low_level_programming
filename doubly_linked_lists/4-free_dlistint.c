230c16530879beeb0ec11fbeec8193dfb2ee1902
Thomas3300000 <8844@holbertonstudents.com>
2024-04-03 10:23:44 +0000

    create and make function free_dlistint

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a dlistint_t list
+ * @head: pointer to the head to the linked list
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
