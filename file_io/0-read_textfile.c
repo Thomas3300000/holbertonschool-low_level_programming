b345cb147e34bf528b6acea18db9e15f94a626ca
Thomas3300000 <8844@holbertonstudents.com>
2024-04-05 21:15:22 +0000

    all files and create and make function read_textfile

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * read_textfile -  function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: pointers to the file
+ * @letters: number of letters toread and print
+ *
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, rd, wr;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	if (op == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	rd = read(op, buffer, letters);
+	if (rd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	wr = write(STDOUT_FILENO, buffer, rd);
+	if (wr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	if (wr != rd)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(op);
+	return (wr);
+}
